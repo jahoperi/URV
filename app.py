5c0ba5491a6df26d180a9df62255794102cfcd7a
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-05-31 13:54:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 import pandas as pd
 import streamlit as st
+
+st.text("hola lllllllllllllll")
+a = "sreeram"
+st.text("ffffffffffffffffffffffffffffffffffffffffff{}.format()")
+
+st.header("fttttttttttttttttttttttttttttttttttttttttttttttttttttt")
+st.subheader("333333333333333333333333333333333333333333333333333")
+st.title("datos públicos de URV")
+st.markdown("wwwwwwwwwwwwwwwwwwwwww")
+
+
+
 df = pd.read_csv("datos.csv")
 
 st.dataframe(df)
