6b6d63bb3a91ce019fbd19cbd9e3d8bde45b6387
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-05-31 09:34:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,1 @@
-import streamlit as st
-import pandas as pd
-from st_aggrid import AgGrid
 
-@st.cache_data()
-def load_data():
-    data = pd.read_csv('./datos.csv', parse_dates=['referenceDate'])
-    return data
-
-data = load_data()
-
-AgGrid(data, height=400)
