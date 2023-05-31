892f1c082d8e9420797bbe0745976f1b2643a793
jahoperi <97061564+jahoperi@users.noreply.github.com>
2023-05-31 08:13:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,46 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 import pandas as pd
 import streamlit as st
+from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid.shared import GridUpdateMode
+
+iris = pd.read_csv(
+    "datos.csv"
+)
+
+def aggrid_interactive_table(df: pd.DataFrame):
+    """Creates an st-aggrid interactive table based on a dataframe.
+
+    Args:
+        df (pd.DataFrame]): Source dataframe
+
+    Returns:
+        dict: The selected row
+    """
+    options = GridOptionsBuilder.from_dataframe(
+        df, enableRowGroup=True, enableValue=True, enablePivot=True
+    )
+
+    options.configure_side_bar()
+
+    options.configure_selection("single")
+    selection = AgGrid(
+        df,
+        enable_enterprise_modules=True,
+        gridOptions=options.build(),
+        theme="light",
+        update_mode=GridUpdateMode.MODEL_CHANGED,
+        allow_unsafe_jscode=True,
+    )
+
+    return selection
 
 
-st.title('CONSAR DATOS URV ACTUALIZADO MATO 2023')
+iris = pd.read_csv(
+    "datos.csv"
+)
 
-df = pd.read_excel(r'datos.xlsx', sheet_name='URV')
-st.table(df)
+selection = aggrid_interactive_table(df=iris)
+
+if selection:
+    st.write("You selected:")
+    st.json(selection["selected_rows"])
